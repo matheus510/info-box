d24057b4a24663cc520923abcb54baf6b04217a5
Matheus Fonseca <matheus510.fonseca@gmail.com>
2018-06-19 21:07:00 -0300

    fix nos conflitos

--- src/App.vue
+++ src/App.vue
@@ -42,13 +42,8 @@
             </v-flex>
             <!-- midia web -->
             <v-flex xs8 offset-xs2 v-if="noticiaAtual.IdMidia === 1">
-<<<<<<< HEAD
-              <v-btn @click="abaAtual = 1" :color="abaAtual === 1 ? 'primary': 'secondary'" class="px-4" value="1">Leitura</v-btn>
-              <v-btn @click="abaAtual = 2" :color="abaAtual === 2 ? 'primary': 'secondary'" class="px-4" value="2">Screenshot</v-btn>
-=======
               <v-btn @click="abaAtual = 1" :color="abaAtual === 1 ? 'white': 'secondary'" class="px-4" value="1">Leitura</v-btn>
               <v-btn @click="abaAtual = 2" :color="abaAtual === 2 ? 'white': 'secondary'" class="px-4" value="2">Screenshot</v-btn>
->>>>>>> 537625bbe725f725dbceef29c500bbac72f83db0
             </v-flex>
             <v-flex xs8 offset-xs2 v-if="noticiaAtual.IdMidia === 1 && abaAtual === 1">
               <v-card class="py-3 px-4 text-xs-left">
@@ -59,17 +54,9 @@
             </v-flex>
             <v-flex xs8 offset-xs2 v-if="noticiaAtual.IdMidia === 1 && abaAtual === 2">
               <v-card class="py-3 px-4 text-xs-left">
-<<<<<<< HEAD
-                <v-card-text class="py-3 justify">
-                  <div class="viewer" v-viewer>
-                    <img :src="noticiaAtual.screenshot" />
-                  </div>
-                </v-card-text>
-=======
                 <div class="viewer" v-viewer>
                   <img :src="noticiaAtual.screenshot" />
                 </div>
->>>>>>> 537625bbe725f725dbceef29c500bbac72f83db0
               </v-card>
             </v-flex>
             <!-- /midia web --> 
@@ -190,131 +177,6 @@
         return moment(this.noticiaAtual.DataHora).format("dddd, D MMMM YYYY, h:mm:ss a")
       }
     },
-<<<<<<< HEAD
-    methods: {
-      getParametrosMvc () {
-        this.urlToken = window.location.href.split('=')[1]
-        
-        return Promise.resolve(services.common.descriptografar(this.urlToken)).then((data) => {
-          this.stringParametros = data
-          const expressao = new RegExp('([^=&?]+)=([^&]+)', 'g')
-          this.arrayParametros = this.stringParametros.match(expressao)
-          this.arrayParametros.map((parametro) => {
-            let nome = parametro.substr(0, parametro.indexOf('='))
-            this.parametros[nome] = parametro.substr(parametro.indexOf('=') + 1, parametro.length)
-          })
-        })
-      },
-      loadPropriedadesMvc () {
-        return Promise.resolve(services.common.getPropriedadesMvc(this.parametros.idProdutoMvc))
-          .then((data) => {
-            this.parametros.idProduto = data.IdProduto
-            this.parametros.propriedadesMvc = data
-            Promise.resolve(services.common.getDadosVisualizacao(this.parametros.idNoticia, this.parametros.idProdutoMvc, this.parametros.idProduto))
-              .then((data) => {
-            let vm = this
-            let dados = data          
-            return setTimeout(function () {
-              vm.parametros.grifos = dados.grifos
-              vm.parametros.fontesRestritas = dados.fontesRestritas
-              vm.parametros.noticiasSimilares = dados.noticiasSimilares
-              vm.parametros.opcoes = dados.opcoes
-              if (vm.parametros.grifos && vm.noticiaAtual.Conteudo) {
-                vm.noticiaAtual.Conteudo = helpers.highlight(vm.noticiaAtual.Conteudo, vm.parametros.grifos)
-              }
-              if (vm.parametros.opcoes['OpcaoExposicaoMesa'] && vm.parametros.opcoes['OpcaoCentimetragemVisualizacaoBook']) {
-                Promise.resolve(services.common.getExposicao(vm.parametros.idProdutoMvc, vm.parametros.idNoticia, vm.noticiaAtual.IdMidia, vm.parametros.opcoes['centimetragemWeb']))
-                  .then((data) => {
-                    vm.noticiaAtual.exposicao = data
-                    vm.items[3].value = vm.noticiaAtual.exposicao.Exposicao.substring(14, vm.noticiaAtual.exposicao.Exposicao.length)
-                  })
-            }
-            if (vm.parametros.opcoes['OpcaoExposicaoPorCanal'] && vm.parametros.opcoes['OpcaoCentimetragemVisualizacaoBook']) {
-                Promise.resolve(services.common.getExposicaoCanal(vm.parametros.idProdutoMvc, vm.parametros.idNoticia, vm.parametros.idMidia))
-                  .then((data) => {
-                    vm.noticiaAtual.exposicao = data
-                    vm.items[3].value = vm.noticiaAtual.exposicao.Exposicao.substring(14, vm.noticiaAtual.exposicao.Exposicao.length)
-                  })
-            }
-            if (vm.parametros.opcoes['OpcaoTiragemVisualizacaoBook']) {
-                Promise.resolve(services.common.getTiragem(vm.parametros.idNoticia))
-                  .then((data) => {
-                    vm.noticiaAtual.tiragem = data
-                    vm.items[4].value = vm.noticiaAtual.tiragem
-                  })
-            }
-            if (vm.parametros.opcoes['OpcaoValoracaoVisualizacaoBook']) {
-                Promise.resolve(services.common.getValoracao(vm.parametros.idProdutoMvc, vm.parametros.idNoticia))
-                  .then((data) => {
-                    vm.noticiaAtual.valoracao = data
-                    vm.items[2].value = vm.noticiaAtual.valoracao ? vm.noticiaAtual.valoracao : 'Informação não disponível' 
-                  })
-            }
-            }, 2000, vm, dados)
-              })
-          })
-      },
-      loadIdNoticiasBook () {
-        if (this.parametros.idBook) {
-          Promise.resolve(services.common.getIdNoticiasBook(this.parametros.idBook))
-            .then((data) => {
-              this.parametros.idNoticiasBook = data
-            })
-        }
-      },
-      loadImpresso() {
-        this.parametros.listaPaginasRecorte = []
-        this.parametros.listaIdsPaginas = []
-        const vm = this
-        Promise.resolve(services.impresso.getIdsPaginas(this.parametros.idNoticia)
-          .then((response) => {
-            vm.parametros.listaIdsPaginas = response.Paginas
-            vm.parametros.listaIdsPaginas.map((pagina, index) => {
-              Promise.resolve(services.impresso.getPaginaComRecortes(pagina)
-                .then((response) => {
-                  vm.parametros.listaPaginasRecorte.push(response)
-                  vm.$forceUpdate()
-                }), vm)
-            })
-            Promise.resolve(services.impresso.getCapa(vm.parametros.idNoticia)
-              .then((data) => {
-                vm.noticiaAtual.capa = data
-                vm.$forceUpdate()
-              }), vm)
-          })
-        , vm)
-      },
-      loadWeb () {
-        debugger
-        Promise.resolve(services.web.getScreenshot(this.parametros.idNoticia).then((response) => {
-            this.noticiaAtual.screenshot = response
-            this.$forceUpdate()
-          })
-        )
-      },
-      loadNoticia () {
-        Promise.resolve(services.common.getNoticia(this.parametros.idProdutoMvc, this.parametros.idNoticia, false))
-          .then((data) => {
-            this.noticiaAtual = data
-            this.items[1].value = moment(this.noticiaAtual.DataHora).format("DD/MM/YYYY")
-            if (this.noticiaAtual.IdMidia === 2) {
-              this.loadImpresso()
-            }
-            if (this.noticiaAtual.IdMidia === 1) {
-              this.loadWeb()
-            }
-          })
-      },
-      inited (viewer) {
-        this.$viewer = viewer
-        this.$viewer.show()
-      },
-      show () {
-        this.$viewer.show()
-      }
-    },
-=======
->>>>>>> 537625bbe725f725dbceef29c500bbac72f83db0
     mounted () {
       const vm = this
       helpers.init(vm)
