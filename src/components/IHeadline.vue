ff5a869140611b653fa80b0fb1ca8a1f77610c8e
Matheus Fonseca <matheus510.fonseca@gmail.com>
2018-06-22 19:53:42 -0300

    pequeno fix no espacamento dos botoes

--- src/components/IHeadline.vue
+++ src/components/IHeadline.vue
@@ -3,7 +3,9 @@
     <span class="headline mb-3">{{ noticiaAtual.Titulo ? noticiaAtual.Titulo : noticiaAtual.TituloOriginal}}</span><br />
     <span class="body-2 mb-3">{{ noticiaAtual.NomeFonte }}</span><span class="body-2" v-if="noticiaAtual.NomeProgramaSecao">{{ ` | ${noticiaAtual.NomeProgramaSecao}` }}</span><br />
     <span class="body-2 mb-3">{{ dataVeiculacao }}</span><br />
-    <v-btn v-for="botao in parametros.botoes" :class="abaAtual === botao.valor ? 'white' : 'secondary'" :key="botao.texto" @click="botao.click(botao)" class="px-4" :value="botao.valor">{{ botao.texto }}</v-btn>
+    <v-flex xs12 class="pt-4">
+      <v-btn v-for="botao in parametros.botoes" :class="abaAtual === botao.valor ? 'white' : 'secondary'" :key="botao.texto" @click="botao.click(botao)" class="px-4" :value="botao.valor">{{ botao.texto }}</v-btn>
+    </v-flex>
   </v-flex>
 </template>
 
